c905c421e00cc515336c9e044946914dc5163cea
calebdaasi <calebdassi@gmail.com>
2026-08-20 17:44:06 +0100

    feat: commande artisan vigilcore:dispatch-periodic-incident pour rotation automatique des 20 services

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Planification automatique de la rotation des 20 services toutes les 15 minutes
+Schedule::command('vigilcore:dispatch-periodic-incident')->everyFifteenMinutes();
